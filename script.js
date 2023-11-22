98082142c826b87e9b994f9d1532b8f3cf282b4d
TALHA SIDDIQUE <116684368+talhasiddique7@users.noreply.github.com>
2023-11-22 12:43:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let count = 0;
+let highestCount = 0;
+let savedData = [];
+
+function updateCount() {
+  const countDisplay = document.getElementById('count');
+  countDisplay.textContent = count;
+}
+
+function increase() {
+  count++;
+  if (count > highestCount) {
+    highestCount = count;
+  }
+  updateCount();
+}
+
+function decrease() {
+    if(count==0){
+        count=0;
+    }else{
+    count--;
+
+    }
+  updateCount();
+}
+
+function saveData() {
+  const userName = prompt('Enter your name:');
+  const description = prompt('Enter a description (optional):');
+  const date = new Date().toLocaleString();
+
+  savedData.push({ userName, description, date, count });
+
+  alert('Data saved successfully!');
+}
+
+function showSavedData() {
+  let dataToShow = '';
+  savedData.forEach((data, index) => {
+    dataToShow += `Entry ${index + 1}: \n`;
+    dataToShow += `Name: ${data.userName}\n`;
+    dataToShow += `Description: ${data.description || 'No description provided'}\n`;
+    dataToShow += `Date: ${data.date}\n`;
+    dataToShow += `Count: ${data.count}\n\n`;
+  });
+
+  alert(dataToShow || 'No saved data found.');
+}
